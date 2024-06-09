debcc19191fd703acf80d2572582b1333e42a00e
dh <dnjsehgns98@gmail.com>
2024-06-09 22:06:15 +0900

    20240609

--- appletv/src/App.jsx
+++ appletv/src/App.jsx
@@ -2,12 +2,18 @@ import './App.css';
 import Banner from './components/MovieModal/Banner';
 import Nav from './components/MovieModal/Nav';
 import styled from 'styled-components';
+import Row from './components/MovieModal/Row';
+import requests from './api/requests';
 
 function App() {
   return (
     <Container>
       <Nav />
       <Banner />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
     </Container>
   );
 }
